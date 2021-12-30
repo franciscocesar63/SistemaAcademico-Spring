5a203c382438973a865182ba64f66b1a4371dd04
franciscocesar63 <franciscocesar888@gmail.com>
2021-12-30 18:53:55 -0300

    Projeto Criado

---
+++ src/test/java/br/com/cesarfilho/sistemaacademico/SistemaacademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesarfilho.sistemaacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaacademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
